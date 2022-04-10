9cea4616a8c4b271efd55d2e66c8e99bc023de6c
Zeke Soto <dsoto@allresco.com>
2022-04-10 12:28:48 -0500

    Food Item: Add Grapes

---
+++ src/main/java/net/zeke5/tutorialmod/item/ModFoodComponents.java
@@ -0,0 +1,8 @@
+package net.zeke5.tutorialmod.item;
+
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+    public static final FoodComponent GRAPE = new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).build();
+
+}
--- src/main/java/net/zeke5/tutorialmod/item/ModItems.java
+++ src/main/java/net/zeke5/tutorialmod/item/ModItems.java
@@ -21,7 +21,10 @@ public class ModItems {
             new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.MYTHRIL).maxDamage(16)));
 
     public static final Item LILAC_FLOWER_BULB = registerItem("lilac_flower_bulb",
-            new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+            new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item GRAPE = registerItem("grape",
+            new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL).food(ModFoodComponents.GRAPE)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
