027bee0edcb97bad655bd35bef7d81cb26c7c829
Zeke Soto <dsoto@allresco.com>
2022-04-10 15:22:17 -0500

    Add Tutorial Client Mod

---
+++ src/main/java/net/zeke5/tutorialmod/TutorialClientMod.java
@@ -0,0 +1,9 @@
+package net.zeke5.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialClientMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
