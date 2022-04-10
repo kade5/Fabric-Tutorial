c52a2079b2d6ffe72ca7b0da78c32e8d0a57113d
Zeke Soto <dsoto@allresco.com>
2022-04-10 15:44:06 -0500

    Custom Tools: Add a full set of custom mythril tools.

--- src/main/java/net/zeke5/tutorialmod/item/ModItems.java
+++ src/main/java/net/zeke5/tutorialmod/item/ModItems.java
@@ -2,10 +2,15 @@ package net.zeke5.tutorialmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.zeke5.tutorialmod.TutorialMod;
 import net.zeke5.tutorialmod.item.custom.DowsingRodItem;
+import net.zeke5.tutorialmod.item.custom.ModAxeItem;
+import net.zeke5.tutorialmod.item.custom.ModHoeItem;
+import net.zeke5.tutorialmod.item.custom.ModPickaxeItem;
 
 public class ModItems {
     public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
@@ -24,7 +29,29 @@ public class ModItems {
             new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
 
     public static final Item GRAPE = registerItem("grape",
-            new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL).food(ModFoodComponents.GRAPE)));
+            new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item MYTHRIL_SWORD = registerItem("mythril_sword",
+            new SwordItem(ModToolMaterials.MYTHRIL, 1, 2f,
+                    new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item MYTHRIL_AXE = registerItem("mythril_axe",
+            new ModAxeItem(ModToolMaterials.MYTHRIL, 3, 1f,
+                    new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item MYTHRIL_HOE = registerItem("mythril_hoe",
+            new ModHoeItem(ModToolMaterials.MYTHRIL, 0, 0f,
+                    new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item MYTHRIL_SHOVEL = registerItem("mythril_shovel",
+            new ShovelItem(ModToolMaterials.MYTHRIL, 0, 0f,
+                    new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item MYTHRIL_PICKAXE = registerItem("mythril_pickaxe",
+            new ModPickaxeItem(ModToolMaterials.MYTHRIL, 1, 1f,
+                    new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
