58c840b12fda91153edd552a172821108c2f10ae
Zeke Soto <dsoto@allresco.com>
2022-04-09 22:46:43 -0500

    Advanced Item: Add Dowsing rod item

--- src/main/java/net/zeke5/tutorialmod/item/ModItems.java
+++ src/main/java/net/zeke5/tutorialmod/item/ModItems.java
@@ -5,6 +5,7 @@ import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.zeke5.tutorialmod.TutorialMod;
+import net.zeke5.tutorialmod.item.custom.DowsingRodItem;
 
 public class ModItems {
     public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
@@ -15,6 +16,9 @@ public class ModItems {
 
     public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
             new Item(new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
+
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod",
+            new DowsingRodItem(new FabricItemSettings().group(ModItemGroup.MYTHRIL).maxDamage(16)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
