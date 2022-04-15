592eec2d88b0f5c5fde4ea1a0f5e5a90eff05261
Zeke Soto <dsoto@allresco.com>
2022-04-15 15:59:55 -0500

    Custom Potion Recipe

--- src/main/java/net/zeke5/tutorialmod/potion/ModPotions.java
+++ src/main/java/net/zeke5/tutorialmod/potion/ModPotions.java
@@ -2,10 +2,13 @@ package net.zeke5.tutorialmod.potion;
 
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.zeke5.tutorialmod.TutorialMod;
 import net.zeke5.tutorialmod.effect.ModEffects;
+import net.zeke5.tutorialmod.item.ModItems;
+import net.zeke5.tutorialmod.mixin.BrewingRecipeRegistryMixin;
 
 public class ModPotions {
 
@@ -18,5 +21,13 @@ public class ModPotions {
 
     public static  void registerPotion() {
         FREEZE_POTION = registerPotion("freeze_potion");
+        registerPotionPotionRecipes();
+
+
+    }
+
+    private static void registerPotionPotionRecipes() {
+        BrewingRecipeRegistryMixin.invokeRegisterPotionRecipe(Potions.AWKWARD, ModItems.MYTHRIL_INGOT,
+                ModPotions.FREEZE_POTION);
     }
 }
